7aacd8f2c4200a264f2b05419200b0dda6883cc9
Sasha <zyrsas@gmail.com>
2019-10-21 12:31:20 +0300

    Initial Commit

---
+++ UranImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UranImages
+//
+//  Created by Sasha on 21.10.2019.
+//  Copyright © 2019 Sasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
